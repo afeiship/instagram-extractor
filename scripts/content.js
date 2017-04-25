c947898c036bbdf5fdf3d54657f25e28340e18a2
afeiship <1290657123@qq.com>
2017-04-25 20:58:28 +0800

    intioa

---
+++ scripts/content.js
@@ -0,0 +1,25 @@
+
+(function($){
+
+  console.log('content ready!');
+
+  function getImgElement(inOvgElement){
+      return inOvgElement.parentElement.querySelector('img');
+  }
+
+  function getVideoElement(inOvgElement){
+      return inOvgElement.parentElement.querySelector('video');
+  }
+
+
+  $(document).on('mouseover','._ovg3g',function(){
+      var el = getImgElement(this);
+      console.log('IMG:->',el.src);
+  });
+
+  $(document).on('mouseover','._c2kdw',function(){
+      var el = getVideoElement(this);
+      console.log('VIDEO:->',el.src);
+  });
+
+}($));
