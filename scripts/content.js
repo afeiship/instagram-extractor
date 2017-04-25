8c1daabbd1d1536b5ed66c627fba5123a71d10da
afeiship <1290657123@qq.com>
2017-04-25 21:20:24 +0800

    optimize

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,20 @@
+console.log('content ready!');
 
-(function($){
+function getImgElement(inOvgElement){
+    return inOvgElement.parentElement.querySelector('img');
+}
 
-  console.log('content ready!');
-
-  function getImgElement(inOvgElement){
-      return inOvgElement.parentElement.querySelector('img');
-  }
-
-  function getVideoElement(inOvgElement){
-      return inOvgElement.parentElement.querySelector('video');
-  }
+function getVideoElement(inOvgElement){
+    return inOvgElement.parentElement.querySelector('video');
+}
 
 
-  $(document).on('mouseover','._ovg3g',function(){
-      var el = getImgElement(this);
-      console.log('IMG:->',el.src);
-  });
+$(document).on('mouseover','._ovg3g',function(){
+    var el = getImgElement(this);
+    console.log('IMG:->',el.src);
+});
 
-  $(document).on('mouseover','._c2kdw',function(){
-      var el = getVideoElement(this);
-      console.log('VIDEO:->',el.src);
-  });
-
-}($));
+$(document).on('mouseover','._c2kdw',function(){
+    var el = getVideoElement(this);
+    console.log('VIDEO:->',el.src);
+});
