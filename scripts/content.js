7954555e552aca55a318a099d0c86fb203b03339
afeiship <1290657123@qq.com>
2018-02-07 18:09:39 +0800

    issue:update

--- scripts/content.js
+++ scripts/content.js
@@ -8,13 +8,19 @@ function getVideoElement(inOvgElement){
     return inOvgElement.parentElement.querySelector('video');
 }
 
+// $(document).on('mouseover',function(){
+//   console.log('mouse over!');
+//   console.log(
+//     $('._2di5p').length
+//   );
+// });
 
-$(document).on('mouseover','._ovg3g',function(){
+$(document).on('mouseover','._8n9ix',function(){
     var el = getImgElement(this);
     console.log('IMG:->',el.src);
 });
 
-$(document).on('mouseover','._c2kdw',function(){
+$(document).on('mouseover','._usfov',function(){
     var el = getVideoElement(this);
     console.log('VIDEO:->',el.src);
 });
